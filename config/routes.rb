023ceafab2fa4eaf16aa04af2fb133ceb8f64404
Fared-daoud <faredjalel@gmail.com>
2016-12-17 11:13:10 +0100

    change the root link

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root 'surveyresponds#new'
   resources :surveyresponds
   resources :surveys
-  resources :surveyresponds
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
